41fc4dc457e5300cbcae5d88342395fddf580986
saswat810 <100258729+saswat810@users.noreply.github.com>
2023-08-21 20:57:10 +0530

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const mulerstorage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, 'uploads');
   },
   filename(req, file, cb) {
     cb(
