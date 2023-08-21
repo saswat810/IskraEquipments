d35d7af3dc4bbbe7b5d5ee69a546e115c0265ed1
saswat810 <100258729+saswat810@users.noreply.github.com>
2023-08-21 18:07:10 +0530

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
   filename(req, file, cb) {
     cb(
       null,
-      Date.now() + file.originalname
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
