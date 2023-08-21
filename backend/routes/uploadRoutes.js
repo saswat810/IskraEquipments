b91a3a95b60dc9fd9c9db675e4158be8195fc12c
saswat810 <100258729+saswat810@users.noreply.github.com>
2023-08-21 15:09:20 +0530

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,41 +1,49 @@
-import path from 'path'
-import express from 'express'
-import multer from 'multer'
-const router = express.Router()
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, 'uploads/');
   },
   filename(req, file, cb) {
     cb(
       null,
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    )
+    );
   },
-})
+});
 
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-  const mimetype = filetypes.test(file.mimetype)
+function fileFilter(req, file, cb) {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
 
   if (extname && mimetype) {
-    return cb(null, true)
+    cb(null, true);
   } else {
-    cb('Images only!')
+    cb(new Error('Images only!'), false);
   }
 }
 
-const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb)
-  },
-})
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single('image');
 
-router.post('/', upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`)
-})
+router.post('/', (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    }
 
-export default router
+    res.status(200).send({
+      message: 'Image uploaded successfully',
+      image: `/${req.file.path}`,
+    });
+  });
+});
+
+export default router;
